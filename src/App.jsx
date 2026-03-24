3e72cd0760e004603b3a6309ecb7eaa34ac389b0
Guedesmavi <mariavitoriaguedesferreira@gmail.com>
2026-03-24 11:56:53 -0300

    card21

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import i17 from "./assets/img/17.png"
 import i18 from "./assets/img/18.png"
 import i19 from "./assets/img/19.png"
 import i20 from "./assets/img/20.png"
+import i21 from "./assets/img/21.png"
 
 const cards = [
   { id: 1, type: 1, titulo: 'O que é react', texto: 'React é uma biblioteca JavaScript de código aberto, focada na criação de interfaces de usuário (UI) interativas e eficientes.', code: i1, tip:"💡Antes de começar o React domine JavaScript moderno."},
@@ -63,6 +64,7 @@ const cards = [
 
   { id: 20, type: 5, titulo: 'O que é useEffect', texto: 'O useEffect é um Hook do React para gerenciar efeitos colaterais (APIs, DOM, timers). Ele executa após a renderização e seu comportamento depende do array de dependências.', code: i20, tip:"💡 Sempre use o array de dependências [] para evitar execuções infinitas. Array vazio [] roda uma vez (montagem), com variáveis [data] roda quando elas mudam, e sem array roda a cada"},
 
+  { id: 21, type: 5, titulo: 'Ciclo básico do react', texto: 'Baseia-se na criação de componentes funcionais que retornam JSX, o ciclo envolve criação e inserção na tela, mudança de estado ou props e remoção da tela, gerenciado pelo Hook useEffect e gerenciamento de estado com useState.', code: i21, tip:"💡 Retornar uma função dentro do useEffect permite limpar subscrições ou timers, evitando vazamentos de memória"},
 ]
 
 function App() {
