7f8224500325d9cbd3ae5903757f0eb544d08cf3
ayla-vilela <ayla.vilela51@gmail.com>
2026-03-24 11:40:57 -0300

    card2

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@
 import Card from './components/Card'
 import './App.css'
 import i1 from "./assets/img/1.png"
+import i2 from "./assets/img/2.png"
 
 const cards = [
   { id: 1, type: 1, titulo: 'O que é react', texto: 'React é uma biblioteca JavaScript de código aberto, focada na criação de interfaces de usuário (UI) interativas e eficientes.', code: i1, tip:"💡Antes de começar o React domine JavaScript moderno."},
+  
+  { id: 2, type: 1, titulo: 'O que é Vite', texto: 'Vite é um automatizador de projetos web focado na velocidade,oferecendo servidor que atualiza intanteniamente,usado por React.', code: i2, tip:"💡Automatizador de projetos focado na VELOCIDADE!"},
 ]
 
 function App() {
